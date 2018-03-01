cb26ce498910d0ac00b3c54e11fc754f10972eac
Srinivas Pericherla <srinivas.pericherla@gmail.com>
2018-02-28 23:18:52 -0500

    First Commit

---
+++ src/main/java/com/example/controller/DemoController.java
@@ -0,0 +1,12 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+	@RequestMapping(value="/hello")
+	public String sayHello() {
+		return "Hello"; 	
+	}
+}
